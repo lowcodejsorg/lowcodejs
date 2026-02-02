7f0639147ea257de9e4dce7d56accbfa45b5872c
indiodev <jhollyfer.fr@gmail.com>
2026-02-02 08:27:26 -0500

    chore: adicionando deploy homolog

--- backend/application/resources/table-rows/update/update.controller.ts
+++ backend/application/resources/table-rows/update/update.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import type { FastifyReply, FastifyRequest } from 'fastify';
 import { Controller, getInstanceByToken, PUT } from 'fastify-decorators';
 
@@ -13,7 +14,6 @@ import { TableRowUpdateParamsValidator } from './update.validator';
 })
 export default class {
   constructor(
-    // eslint-disable-next-line no-unused-vars
     private readonly useCase: TableRowUpdateUseCase = getInstanceByToken(
       TableRowUpdateUseCase,
     ),
