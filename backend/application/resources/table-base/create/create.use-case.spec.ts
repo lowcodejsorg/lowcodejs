d09429fe9da419a74abbcafdea1cff4c3710faed
indiodev <jhollyfer.fr@gmail.com>
2026-02-09 05:38:37 -0500

    campos nativos e bulk de registros (lixeira e restauracao)

--- backend/application/resources/table-base/create/create.use-case.spec.ts
+++ backend/application/resources/table-base/create/create.use-case.spec.ts
@@ -5,17 +5,20 @@ import {
   E_TABLE_STYLE,
   E_TABLE_VISIBILITY,
 } from '@application/core/entity.core';
+import FieldInMemoryRepository from '@application/repositories/field/field-in-memory.repository';
 import TableInMemoryRepository from '@application/repositories/table/table-in-memory.repository';
 
 import TableCreateUseCase from './create.use-case';
 
+let fieldInMemoryRepository: FieldInMemoryRepository;
 let tableInMemoryRepository: TableInMemoryRepository;
 let sut: TableCreateUseCase;
 
 describe('Table Create Use Case', () => {
   beforeEach(() => {
     tableInMemoryRepository = new TableInMemoryRepository();
-    sut = new TableCreateUseCase(tableInMemoryRepository);
+    fieldInMemoryRepository = new FieldInMemoryRepository();
+    sut = new TableCreateUseCase(tableInMemoryRepository, fieldInMemoryRepository);
   });
 
   it('deve criar tabela com sucesso', async () => {
