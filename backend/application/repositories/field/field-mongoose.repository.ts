e7f8e9cac897dd17203bc43e0d0f02c4bce66f64
Vanessa Lopes <80737823+VanessaLops@users.noreply.github.com>
2026-03-19 08:23:42 -0300

    Atualizar o field-mongoose.repository.ts
    Delete function hardDelete fix

--- backend/application/repositories/field/field-mongoose.repository.ts
+++ backend/application/repositories/field/field-mongoose.repository.ts
@@ -106,10 +106,7 @@ export default class FieldMongooseRepository implements FieldContractRepository 
     await Model.deleteOne({ _id });
   }
 
-  async hardDelete(_id: string): Promise<void> {
-    await Model.deleteOne({ _id });
-  }
-
+  
   async count(payload?: FieldQueryPayload): Promise<number> {
     const where = this.buildWhereClause(payload);
     return Model.countDocuments(where);
