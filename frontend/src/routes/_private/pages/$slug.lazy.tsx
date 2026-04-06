edcc7d78a29cb5c325f9cfba4008f0da49d24208
indiodev <jhollyfer.fr@gmail.com>
2026-04-06 06:53:43 -0500

    fix de navegação nao bloqueante tabelas

--- frontend/src/routes/_private/pages/$slug.lazy.tsx
+++ frontend/src/routes/_private/pages/$slug.lazy.tsx
@@ -1,32 +1,3 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { createLazyFileRoute, useParams } from '@tanstack/react-router';
+import { createLazyFileRoute } from '@tanstack/react-router';
 
-import { PageShell } from '@/components/common/page-shell';
-import { ContentViewer } from '@/components/common/rich-editor/viewer';
-import { pageDetailOptions } from '@/hooks/tanstack-query/_query-options';
-
-export const Route = createLazyFileRoute('/_private/pages/$slug')({
-  component: RouteComponent,
-});
-
-function RouteComponent(): React.JSX.Element {
-  const { slug } = useParams({
-    from: '/_private/pages/$slug',
-  });
-
-  const { data: page } = useSuspenseQuery(pageDetailOptions(slug));
-
-  return (
-    <PageShell data-test-id="custom-page">
-      <PageShell.Header>
-        <h1 className="text-2xl font-medium ">{page?.name ?? ''}</h1>
-      </PageShell.Header>
-
-      <PageShell.Content>
-        <ContentViewer content={page?.html ?? ''} />
-      </PageShell.Content>
-
-      <PageShell.Footer />
-    </PageShell>
-  );
-}
+export const Route = createLazyFileRoute('/_private/pages/$slug')({});
--- frontend/src/routes/_private/pages/$slug.tsx
+++ frontend/src/routes/_private/pages/$slug.tsx
@@ -1,12 +1,38 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { createFileRoute, useParams } from '@tanstack/react-router';
 
 import { PageSkeleton } from './-page-skeleton';
 
+import { PageShell } from '@/components/common/page-shell';
+import { ContentViewer } from '@/components/common/rich-editor/viewer';
 import { pageDetailOptions } from '@/hooks/tanstack-query/_query-options';
 
 export const Route = createFileRoute('/_private/pages/$slug')({
   pendingComponent: PageSkeleton,
+  component: RouteComponent,
   loader: ({ context, params }) => {
     context.queryClient.prefetchQuery(pageDetailOptions(params.slug));
   },
 });
+
+function RouteComponent(): React.JSX.Element {
+  const { slug } = useParams({
+    from: '/_private/pages/$slug',
+  });
+
+  const { data: page } = useSuspenseQuery(pageDetailOptions(slug));
+
+  return (
+    <PageShell data-test-id="custom-page">
+      <PageShell.Header>
+        <h1 className="text-2xl font-medium ">{page?.name ?? ''}</h1>
+      </PageShell.Header>
+
+      <PageShell.Content>
+        <ContentViewer content={page?.html ?? ''} />
+      </PageShell.Content>
+
+      <PageShell.Footer />
+    </PageShell>
+  );
+}
