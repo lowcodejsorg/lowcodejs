767e77d41ac38f50715a7e9dc38313c1f2000ced
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2026-05-01 17:45:52 -0500

    adicionando drop source

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -6,8 +6,8 @@ echo "🔀 Verificando migração dual-connection..."
 if [ -f "/app/database/migrations/migrate-dual-connection.ts" ]; then
   su-exec 1001:1001 npm run migrate:dual-connection
 else
-  # su-exec 1001:1001 node /app/database/migrations/migrate-dual-connection.js
-  su-exec 1001:1001 node /app/database/migrations/migrate-dual-connection.js --drop-source
+  su-exec 1001:1001 node /app/database/migrations/migrate-dual-connection.js
+  # su-exec 1001:1001 node /app/database/migrations/migrate-dual-connection.js --drop-source
 fi
 
 echo "🌱 Rodando seeders..."
